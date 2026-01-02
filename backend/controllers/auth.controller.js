acbe5165a6d47100a2a3e58d25201c71014023bf
voradhruv27 <voradhruv09@gmail.com>
2026-01-02 17:22:31 +0530

    updated auth controller and user model

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,10 +4,11 @@ const jwt = require("jsonwebtoken");
 
 // REGISTER
 exports.register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { email, password } = req.body;
 
-  if (!name || !email || !password)
+  if (!email || !password)
     return res.status(400).json({ message: "All fields required" });
+
 
   const userExists = await User.findOne({ email });
   if (userExists)
@@ -16,7 +17,6 @@ exports.register = async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = await User.create({
-    name,
     email,
     password: hashedPassword,
   });
