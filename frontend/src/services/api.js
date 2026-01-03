60340b12276b9050ec6a3cd5fd1171888e68f9c2
voradhruv27 <voradhruv09@gmail.com>
2026-01-03 13:44:19 +0530

    Update: .env file for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create Axios instance with base URL
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://primetrade-assignment-rcfn.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
