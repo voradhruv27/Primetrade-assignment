70770a727c0b959523fe2bbe40fc114100bd2806
voradhruv27 <voradhruv09@gmail.com>
2026-01-03 13:05:39 +0530

    Edited package.json scripts for production

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("Backend is running successfully 🚀");
+});
+
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/posts", require("./routes/post.routes"));
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-app.get("/", (req, res) => {
-  res.status(200).send("Backend is running successfully 🚀");
-});
